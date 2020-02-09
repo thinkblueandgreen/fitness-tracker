17a15f754b008f9aab57f4f83777f1ba7ad61b7b
Mathi <arline.mathivadhani@gmail.com>
2020-02-08 20:19:29 -0800

    added MONGODB_URI

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.JAWSDB-URL || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB-URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
